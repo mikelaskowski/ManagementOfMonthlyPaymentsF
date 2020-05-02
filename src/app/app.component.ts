43ab8b8ffa48f53c6af53a0690709caa4d910ad6
Michal Laskowski <mikelaskowski@o2.pl>
2020-05-02 14:26:45 +0200

    configure display header and header elements depends on isAuthenticated value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private isAuthenticated: boolean;
+
+  constructor(private authService: AuthService){}
+
+
+  ngOnInit(){
+    this.authService.loggedUser.subscribe((user) =>{
+      this.isAuthenticated=!!user;
+    });
+
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
+
+    private isAuthenticated: boolean;
+
+  constructor(private authService: AuthService){}
+
+
+  ngOnInit(){
+    this.authService.loggedUser.subscribe((user) =>{
+      this.isAuthenticated=!!user;
+    });
+
+  }
 
 }
